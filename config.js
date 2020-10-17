bb11589efc90ab4838cd1136ea4630636ff73ce1
tecunMartin <tecunmartin132@gmail.com>
2020-10-17 02:05:21 -0600

    Creamos variables de entorno para now

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ module.exports = {
     secret: process.env.JWT_SECRET,
   },
   mysql: {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.MYSQL_HOST || '',
+    user: process.env.MYSQL_USER || '',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || '',
   },
   mysqlService: {
     port: process.env.MYSQL_SRV_PORT || 3001,
